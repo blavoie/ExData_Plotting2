531cbe88bec8b36e10be150f5293fb6367a55ba5
Bruno Lavoie <bl@brunol.com>
2015-11-20 08:40:41 -0500

    Add plot 5 & 6.

--- plot5.R
+++ plot5.R
@@ -10,3 +10,36 @@ if (!exists("MEI")) {
 if (!exists("SCC")) {
   SCC <- load.SCC()
 }
+
+# How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City?
+
+library(dplyr)
+library(ggplot2)
+
+SCC.veh.which <- which(grepl("vehicle", SCC$EI.Sector, ignore.case = TRUE))
+SCC.veh <- SCC[SCC.veh.which,]
+rm(SCC.veh.which)  
+
+NEI.veh <- NEI %>%
+  filter(fips == "24510") %>%
+  inner_join(SCC.veh, by = "SCC") %>%
+  group_by(year) %>%
+  summarise(emissions=sum(Emissions))
+
+p5<- 
+  qplot(data = NEI.veh, 
+        x=year, y=emissions, 
+        geom = c("point", "line"),
+        main = "Motor vehicule emissions for Baltimore City", 
+        xlab = "Year", 
+        ylab = "Emissions in tons")
+
+ggsave(plot = p5, filename = "plot5.png")
+
+# Cleanup
+
+rm(p5)
+rm(SCC.veh)
+rm(NEI.veh)
+
+
--- plot6.R
+++ plot6.R
@@ -10,3 +10,38 @@ if (!exists("MEI")) {
 if (!exists("SCC")) {
   SCC <- load.SCC()
 }
+
+# Compare emissions from motor vehicle sources in Baltimore City with emissions from motor 
+# vehicle sources in Los Angeles County, California (fips == "06037"). Which city has seen 
+# greater changes over time in motor vehicle emissions?
+
+library(dplyr)
+library(ggplot2)
+
+SCC.veh.which <- which(grepl("vehicle", SCC$EI.Sector, ignore.case = TRUE))
+SCC.veh <- SCC[SCC.veh.which,]
+rm(SCC.veh.which)  
+
+NEI.veh <- NEI %>%
+  filter(fips == "24510" | fips == "06037") %>%
+  inner_join(SCC.veh, by = "SCC") %>%
+  group_by(year, fips) %>%
+  summarise(emissions=sum(Emissions)) %>%
+  mutate(city = ifelse(fips == "24510", "Baltimore City", "Los Angeles County"))
+
+p6 <- qplot(year, emissions, 
+      data = NEI.veh, 
+      color = city, 
+      geom = c("point","line"),
+      main = "Emission from motor vehicle sources",
+      xlab = "Year", 
+      ylab = "Emmissions in tons")
+
+ggsave(plot = p6, filename = "plot6.png")
+
+# Cleanup
+
+rm(p6)
+rm(SCC.veh)
+rm(NEI.veh)
+
